ffbf163fbdeac2f64f122f2064c42129d43a273b
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-09-12 16:21:34 +0100

    FR-515 added ability to specify seach terms on bucket retrieval

--- bucket.js
+++ bucket.js
@@ -13,25 +13,42 @@ var Bucket = function(attributes) {
   this.id = this.attributes.id;
 };
 
-Bucket.get = function(bucket_id, case_type_code) {
+const addAdditionalSearchTermsToURL = (url, searchField, searchTerm) => {
+  if (searchField!==undefined && searchTerm!==undefined) {
+    if (url.indexOf('?')<0) {
+      url += '?'
+    } else {
+      url += '&'
+    }
+    url = url + "search_field=" + encodeURIComponent(searchField) + "&search_term=" + encodeURIComponent(searchTerm)
+  }
+  return url
+}
+
+Bucket.get = function(bucket_id, case_type_code, searchField, searchTerm) {
   if (!Bucket.Caseblocks)
     throw new Error("Must call Caseblocks.setup");
+
   return Q.fcall(function(data) {
-    return rest.get(Bucket.Caseblocks.buildUrl("/case_blocks/buckets/" + bucket_id),  {headers: {"Accept": "application/json"}}).then(function(data) {
+    const url = addAdditionalSearchTermsToURL("/case_blocks/buckets/" + bucket_id, searchField, searchTerm)
+    return rest.get(Bucket.Caseblocks.buildUrl(url),  {headers: {"Accept": "application/json"}}).then(function(data) {
       bucket_data = data["bucket"]
-      bucket_data["case_type_code"] = case_type_code
-      return new Bucket(bucket_data);
-    });
-  });
-};
+      bucket_data.case_type_code = case_type_code
+      bucket_data.searchField = searchField
+      bucket_data.searchTerm = searchTerm
+      return new Bucket(bucket_data)
+    })
+  })
+}
 
 Bucket.prototype.stats = function() {
   if (!Bucket.Caseblocks)
     throw new Error("Must call Caseblocks.setup");
 
-  var _this = this;
+  let _this = this;
   return Q.fcall(function(data) {
-    return rest.get(Bucket.Caseblocks.buildUrl("/case_blocks/bucket_stats/" + _this.id), {headers: {"Accept": "application/json"}}).then(function(data) {
+    const url = addAdditionalSearchTermsToURL("/case_blocks/bucket_stats/" + _this.id, _this.attributes.searchField, _this.attributes.searchTerm)
+    return rest.get(Bucket.Caseblocks.buildUrl(url), {headers: {"Accept": "application/json"}}).then(function(data) {
       return data;
     });
   });
@@ -40,7 +57,7 @@ Bucket.prototype.stats = function() {
 Bucket.prototype.cases = function(page, pageSize) {
   if (!Bucket.Caseblocks)
     throw new Error("Must call Caseblocks.setup");
-  var _this = this;
+  let _this = this;
   return Q.fcall(function(data) {
     if (typeof page == "undefined") {
       page = 0
@@ -48,8 +65,8 @@ Bucket.prototype.cases = function(page, pageSize) {
     if (typeof pageSize == "undefined") {
       pageSize = 10
     }
-    var url = Bucket.Caseblocks.buildUrl("/case_blocks/" + _this.case_type_code + "?bucket_id=" + _this.id + "&page=" + page + "&page_size=" + pageSize)
-    return rest.get(url, {headers: {"Accept": "application/json"}}).then(function(data) {
+    const url = addAdditionalSearchTermsToURL("/case_blocks/" + _this.case_type_code + "?bucket_id=" + _this.id + "&page=" + page + "&page_size=" + pageSize, _this.attributes.searchField, _this.attributes.searchTerm) 
+    return rest.get(Bucket.Caseblocks.buildUrl(url), {headers: {"Accept": "application/json"}}).then(function(data) {
       cases = []
       for (kase of data[_this.case_type_code]) {
         cases.push(new Case(kase))
